79d1bc5d42f6971f2fc6cfa5cf7df1b6933ba327
Mark Healey <markhealey@gmail.com>
2020-05-15 09:06:09 -0600

    Add temp redirect
    temp redirect to this "error", we don't have a "home page" for this app.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
-  res.render("index", { title: "Just Good Advice" });
+  // res.render("index", { title: "Just Good Advice" });
+
+  // temp redirect to this "error", we don't have a "home page" for this app.
+  console.warn("temp redirect to this \"error\", we don't have a \"home page\" for this app.");
+  return res.redirect("https://justgoodadvice.github.io/advicebuilder-status/H10.html");
 });
 
 module.exports = router;
