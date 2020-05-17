cf74fdaf5f9230901573183e60336cfa96f97ed5
Mark Healey <markhealey@gmail.com>
2020-05-17 11:55:12 -0600

    Add redirect handler

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const qs = require("querystring");
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
@@ -9,5 +10,10 @@ router.get("/", (req, res, next) => {
   console.warn("temp redirect to this \"error\", we don't have a \"home page\" for this app.");
   return res.redirect("https://justgoodadvice.github.io/advicebuilder-status/H10.html");
 });
+
+// Handle redirects from classic `app` URL to this new format
+router.get("/_/advice/widget/:adviceSetId/_/:config?/", (req, res, next) => {
+  return res.redirect(`/s/${req.params.adviceSetId}?${qs.stringify(req.query)}`);
+})
 
 module.exports = router;
